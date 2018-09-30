55404cd518ff7cc535464fff4cd78c2eaf00b859
Oling Cat <olingcat@gmail.com>
2018-09-30 22:15:50 +0800

    fix a bug for last paragraph

--- src/extension.ts
+++ src/extension.ts
@@ -13,81 +13,81 @@ import TextEditor = vscode.TextEditor;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = Commands.registerTextEditorCommand('extension.duplicateForTrans', () => {
-
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable =
+      Commands.registerTextEditorCommand('extension.duplicateForTrans', () => {
         // Get the current text editor
         let editor = Window.activeTextEditor;
         if (!editor) {
-            Window.showInformationMessage('Open a file first to manipulate text selections');
-            return;
+          Window.showInformationMessage(
+              'Open a file first to manipulate text selections');
+          return;
         }
         duplicateForTrans(editor);
-    });
+      });
 
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 function duplicateForTrans(editor: TextEditor) {
-    let currentRange = getCurrentRange(editor);
-    let currentText = editor.document.getText(currentRange);
-    let newText = "\n\n\n" + currentText;
+  let currentRange = getCurrentRange(editor);
+  let currentText = editor.document.getText(currentRange);
+  let newText = '\n\n' + currentText;
 
-    // Make changes for docuemnt
-    editor.edit((e) => {
-        e.insert(currentRange.end, newText);
-    })
-    editor.selection = new vscode.Selection(currentRange.start, currentRange.end);
-    editor.revealRange(currentRange);
-    Commands.executeCommand('editor.action.addCommentLine');
-    Commands.executeCommand('cursorMove', {
-        to: "down",
-        by: "line",
-        select: false,
-        value: 2
-    })
+  // Make changes for docuemnt
+  editor.edit((e) => {
+    e.insert(currentRange.end, newText);
+  })
+  editor.selection = new vscode.Selection(currentRange.start, currentRange.end);
+  editor.revealRange(currentRange);
+  Commands.executeCommand('editor.action.addCommentLine');
+  Commands.executeCommand(
+      'cursorMove', {to: 'down', by: 'line', select: false, value: 2})
 }
 
 function getCurrentRange(editor: TextEditor): Range {
+  // Get current document and cursor position
+  let doc = editor.document;
 
-    // Get current document and cursor position
-    let doc = editor.document;
+  let regexpTable = {
+    [vscode.EndOfLine.LF]: /\n\n+/gm,
+    [vscode.EndOfLine.CRLF]: /\r\n(\r\n)+/gm
+  }
 
-    let regexpTable = {
-        [vscode.EndOfLine.LF]: /\n\n+/gm,
-        [vscode.EndOfLine.CRLF]: /\r\n(\r\n)+/gm
-    }
+  let regexp = regexpTable[doc.eol];
 
-    let regexp = regexpTable[doc.eol];
+  let docContent = doc.getText();
+  let eofPosition = doc.positionAt(docContent.length - 1);
+  let cursorPosition: Position = editor.selection.active;
 
-    let docContent = doc.getText();
-    let cursorPosition: Position = editor.selection.active;
+  // Initial some holders
+  let currentParagraph: Range;
 
-    // Initial some holders
-    let currentParagraph: Range;
-
-    // Get paragraphs and selections
-    let selectedText: Selection[];
-    if (!editor.selection.isEmpty) {
-        selectedText = editor.selections;
-        currentParagraph = new Range(selectedText[0].start, selectedText[0].end);
+  // Get paragraphs and selections
+  let selectedText: Selection[];
+  if (!editor.selection.isEmpty) {  // unselected
+    selectedText = editor.selections;
+    currentParagraph = new Range(selectedText[0].start, selectedText[0].end);
+    return currentParagraph;
+  } else {
+    let match;
+    let start = 0;
+    while ((match = regexp.exec(docContent)) !== null) {
+      let p = new Range(doc.positionAt(start), doc.positionAt(match.index));
+      if (cursorPosition.isAfterOrEqual(p.start) &&
+          cursorPosition.isBeforeOrEqual(p.end)) {
+        currentParagraph = p;
         return currentParagraph;
-    } else {
-        let match;
-        for (let start = 0; (match = regexp.exec(docContent)) !== null; start = regexp.lastIndex) {
-            let p = new Range(doc.positionAt(start), doc.positionAt(match.index));
-            if (cursorPosition.isAfterOrEqual(p.start) && cursorPosition.isBeforeOrEqual(p.end)) {
-                currentParagraph = p;
-                return currentParagraph;
-            }
-        }
+      }
+      start = regexp.lastIndex
     }
-    return new Range(cursorPosition, cursorPosition);
+    // for last paragraph
+    currentParagraph = new Range(doc.positionAt(start), eofPosition);
+    return currentParagraph;
+  }
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}

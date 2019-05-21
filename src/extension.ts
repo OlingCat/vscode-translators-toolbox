8161e0208b6a1dcd62942d21d9b37ced93839209
Oling Cat <olingcat@gmail.com>
2019-05-21 18:59:30 +0800

    Add customed paragraph delimiters.

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
 
 import Window = vscode.window;
 import Commands = vscode.commands;
@@ -16,7 +17,7 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable =
+  let dupForTrans =
       Commands.registerTextEditorCommand('extension.duplicateForTrans', () => {
         // Get the current text editor
         let editor = Window.activeTextEditor;
@@ -28,7 +29,7 @@ export function activate(context: vscode.ExtensionContext) {
         duplicateForTrans(editor);
       });
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(dupForTrans);
 }
 
 function duplicateForTrans(editor: TextEditor) {
@@ -36,20 +37,36 @@ function duplicateForTrans(editor: TextEditor) {
   let currentText = editor.document.getText(currentRange);
   let newText = '\n\n' + currentText;
 
-  // Make changes for docuemnt
-  editor.edit((e) => {
-    e.insert(currentRange.end, newText);
-  })
-  editor.selection = new vscode.Selection(currentRange.start, currentRange.end);
-  editor.revealRange(currentRange);
-  Commands.executeCommand('editor.action.addCommentLine');
-  Commands.executeCommand(
-      'cursorMove', {to: 'down', by: 'line', select: false, value: 2})
+  let uri = editor.document.uri
+  let fileName = editor.document.fileName;
+  let fileExt = path.extname(fileName).slice(1);
+  let encloseDelimiters: any = vscode.workspace.getConfiguration('', uri).get('trans.enclose');
+
+  // enclose with customed delimiters or just comment it
+  if (fileExt in encloseDelimiters) {
+    editor.edit((e) => {
+      e.insert(currentRange.start, encloseDelimiters[fileExt]["start"] + "\n");
+      e.insert(currentRange.end, "\n" + encloseDelimiters[fileExt]["end"]);
+      // append duplicated text
+      e.insert(currentRange.end, newText);
+    });
+    Commands.executeCommand('cursorMove', { to: 'down', by: 'line', select: false, value: 4 });
+  } else {
+    // append duplicated text
+    editor.edit((e) => {
+      e.insert(currentRange.end, newText);
+    });
+    editor.selection = new vscode.Selection(currentRange.start, currentRange.end);
+    editor.revealRange(currentRange);
+    Commands.executeCommand('editor.action.addCommentLine');
+    Commands.executeCommand('cursorMove', { to: 'down', by: 'line', select: false, value: 2 });
+  }
 }
 
 function getCurrentRange(editor: TextEditor): Range {
-  // Get current document and cursor position
+  // get current document and cursor position
   let doc = editor.document;
+  let cursorPosition: Position = editor.selection.active;
 
   let regexpTable = {
     [vscode.EndOfLine.LF]: /\n\n+/gm,
@@ -60,22 +77,30 @@ function getCurrentRange(editor: TextEditor): Range {
 
   let docContent = doc.getText();
   let eofPosition = doc.positionAt(docContent.length - 1);
-  let cursorPosition: Position = editor.selection.active;
 
-  // Initial some holders
+  // initial some holders
   let currentParagraph: Range;
 
-  // Get paragraphs and selections
+  // get paragraphs and selections
   let selectedText: Selection[];
-  if (!editor.selection.isEmpty) {  // unselected
+  if (!editor.selection.isEmpty) {  // for selected
     selectedText = editor.selections;
     currentParagraph = new Range(selectedText[0].start, selectedText[0].end);
     return currentParagraph;
   } else {
     let match;
     let start = 0;
+
+    // move down to next non-empty line
+    while (doc.lineAt(cursorPosition).isEmptyOrWhitespace &&
+      cursorPosition.line !== doc.lineCount - 1) {
+      cursorPosition = cursorPosition.translate(1);
+    }
+
+    // find current paragraph
     while ((match = regexp.exec(docContent)) !== null) {
       let p = new Range(doc.positionAt(start), doc.positionAt(match.index));
+      // cursor is in current paragraph
       if (cursorPosition.isAfterOrEqual(p.start) &&
           cursorPosition.isBeforeOrEqual(p.end)) {
         currentParagraph = p;
@@ -83,6 +108,7 @@ function getCurrentRange(editor: TextEditor): Range {
       }
       start = regexp.lastIndex
     }
+
     // for last paragraph
     currentParagraph = new Range(doc.positionAt(start), eofPosition);
     return currentParagraph;

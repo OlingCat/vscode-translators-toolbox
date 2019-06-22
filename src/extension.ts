3dc25ff7a0c751c0be0a9a244d2fa10c74023b60
Oling Cat <olingcat@gmail.com>
2019-06-22 16:03:20 +0800

    Clean lints

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import * as path from 'path';
+import * as vscode from "vscode";
+import * as path from "path";
 
 import Window = vscode.window;
 import Commands = vscode.commands;
@@ -17,13 +17,13 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-  let dupForTrans =
-      Commands.registerTextEditorCommand('extension.duplicateForTrans', () => {
+  const dupForTrans =
+      Commands.registerTextEditorCommand("extension.duplicateForTrans", () => {
         // Get the current text editor
-        let editor = Window.activeTextEditor;
+        const editor = Window.activeTextEditor;
         if (!editor) {
           Window.showInformationMessage(
-              'Open a file first to manipulate text selections');
+              "Open a file first to manipulate text selections");
           return;
         }
         duplicateForTrans(editor);
@@ -33,24 +33,25 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 function duplicateForTrans(editor: TextEditor) {
-  let currentRange = getCurrentRange(editor);
-  let currentText = editor.document.getText(currentRange);
-  let newText = '\n\n' + currentText;
+  const currentRange = getCurrentRange(editor);
+  const currentText = editor.document.getText(currentRange);
+  const newText = "\n\n" + currentText;
 
-  let uri = editor.document.uri
-  let fileName = editor.document.fileName;
-  let fileExt = path.extname(fileName).slice(1);
-  let encloseDelimiters: any = vscode.workspace.getConfiguration('', uri).get('trans.enclose');
+  const uri = editor.document.uri;
+  const fileName = editor.document.fileName;
+  const fileExt = path.extname(fileName).slice(1);
+  const encloseDelimiters: any =
+      vscode.workspace.getConfiguration("", uri).get("trans.enclose");
 
   // enclose with customed delimiters or just comment it
   if (fileExt in encloseDelimiters) {
     editor.edit((e) => {
-      e.insert(currentRange.start, encloseDelimiters[fileExt]["start"] + "\n");
-      e.insert(currentRange.end, "\n" + encloseDelimiters[fileExt]["end"]);
+      e.insert(currentRange.start, encloseDelimiters[fileExt].start + "\n");
+      e.insert(currentRange.end, "\n" + encloseDelimiters[fileExt].end);
       // append duplicated text
       e.insert(currentRange.end, newText);
     });
-    Commands.executeCommand('cursorMove', { to: 'down', by: 'line', select: false, value: 4 });
+    Commands.executeCommand("cursorMove", { to: "down", by: "line", select: false, value: 4 });
   } else {
     // append duplicated text
     editor.edit((e) => {
@@ -58,25 +59,25 @@ function duplicateForTrans(editor: TextEditor) {
     });
     editor.selection = new vscode.Selection(currentRange.start, currentRange.end);
     editor.revealRange(currentRange);
-    Commands.executeCommand('editor.action.addCommentLine');
-    Commands.executeCommand('cursorMove', { to: 'down', by: 'line', select: false, value: 2 });
+    Commands.executeCommand("editor.action.addCommentLine");
+    Commands.executeCommand("cursorMove", { to: "down", by: "line", select: false, value: 2 });
   }
 }
 
 function getCurrentRange(editor: TextEditor): Range {
   // get current document and cursor position
-  let doc = editor.document;
+  const doc = editor.document;
   let cursorPosition: Position = editor.selection.active;
 
-  let regexpTable = {
+  const regexpTable = {
     [vscode.EndOfLine.LF]: /\n\n+/gm,
     [vscode.EndOfLine.CRLF]: /\r\n(\r\n)+/gm
-  }
+  };
 
-  let regexp = regexpTable[doc.eol];
+  const regexp = regexpTable[doc.eol];
 
-  let docContent = doc.getText();
-  let eofPosition = doc.positionAt(docContent.length - 1);
+  const docContent = doc.getText();
+  const eofPosition = doc.positionAt(docContent.length - 1);
 
   // initial some holders
   let currentParagraph: Range;
@@ -99,14 +100,14 @@ function getCurrentRange(editor: TextEditor): Range {
 
     // find current paragraph
     while ((match = regexp.exec(docContent)) !== null) {
-      let p = new Range(doc.positionAt(start), doc.positionAt(match.index));
+      const p = new Range(doc.positionAt(start), doc.positionAt(match.index));
       // cursor is in current paragraph
       if (cursorPosition.isAfterOrEqual(p.start) &&
           cursorPosition.isBeforeOrEqual(p.end)) {
         currentParagraph = p;
         return currentParagraph;
       }
-      start = regexp.lastIndex
+      start = regexp.lastIndex;
     }
 
     // for last paragraph
@@ -116,4 +117,5 @@ function getCurrentRange(editor: TextEditor): Range {
 }
 
 // this method is called when your extension is deactivated
+// tslint:disable-next-line: no-empty
 export function deactivate() {}
